5b06ef32059401a2b64961216abae446d09bb453
Adrien Corvisier <adrien.corvisier@yachtsolutions.fr>
2022-12-06 09:50:18 +0100

    wip

--- Day3/day3.js
+++ Day3/day3.js
@@ -5,6 +5,7 @@ let totalPart1 = 0;
 
 const splittedData = data.split('\n')
 splittedData.pop()
+
 splittedData.map(backpack => {
 
   let firstPart = backpack.slice(0, backpack.length / 2)
@@ -19,11 +20,33 @@ splittedData.map(backpack => {
         commonItemFound = true
     }
   })
-
-
-
-
 })
 
-
 console.log('Part 1: ' + totalPart1)
+
+let totalPart2 = 0
+
+const chunks = [];
+let index = 0;
+let sizeSlice = 3
+while (index < splittedData.length) {
+  chunks.push(splittedData.slice(index, index + sizeSlice));
+  index += sizeSlice;
+}
+
+chunks.map(chunk => {
+  let badgeFound = false;
+
+  chunk.map(backpack => {
+
+      Array.from(backpack).map(item => {
+        if ( !badgeFound && Array.from(chunk)[1].indexOf(item) > -1 && Array.from(chunk)[2].indexOf(item) > -1) {
+          totalPart2 += score.indexOf(item) + 1
+          badgeFound = true
+        }
+      })
+
+  })
+
+})
+console.log('Part 2: ' + totalPart2)
