255f2e8f1585b023b222bdafa62c6ae6e893f5d4
Adrien Corvisier <adrien.corvisier@yachtsolutions.fr>
2022-12-07 21:52:56 +0100

    wip

--- Day4/day4.js
+++ Day4/day4.js
@@ -40,12 +40,11 @@ splittedData.map(pairOfZones => {
   (min2 < max1) ||
   (min1 > max2)
   )
-  //test commit
 
   if(
     (min1 >= min2 && max1 <= max2) ||
     (min2 >= min1 && max2 <= max1) ||
-    (min2 < max1) ||
+    (min2 <= max1 && max2 <= min1) ||
     (min1 > max2)
   ) {
     totalPart2++
